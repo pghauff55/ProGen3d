6546bd2d8018c70cd87649aec02636a32b803db8
Pamela Hauff <pamela.hauff@gmail.com>
2023-11-29 18:30:01 +1000

    Delete ExtendedCMath.h

--- ExtendedCMath.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace mathS {
-	double double_less(double a, double b);
-	double double_greater(double a, double b);
-	double double_lesseq(double a, double b);
-	double double_greatereq(double a, double b);
-}
